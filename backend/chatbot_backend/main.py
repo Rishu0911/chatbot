2acc6ba89759ce3d3783e9a2cff382ce5c0efcb9
Rimjhi <rimjhimkant006@gmail.com>
2025-04-19 13:24:12 +0530

    initial frontend setup

---
+++ backend/chatbot_backend/main.py
@@ -0,0 +1,11 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "gpt2"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+def generate_response(prompt):
+    inputs = tokenizer.encode(prompt, return_tensors="pt")
+    outputs = model.generate(inputs, max_length=100, num_return_sequences=1, no_repeat_ngram_size=2)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return response
